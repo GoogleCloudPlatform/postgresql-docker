94c26468f841d4e51f36dadd970e019a10239876
Wojciech Grzelak <wgrzelak@google.com>
2017-03-24 08:32:41 -0700

    Add licence and tags
    Change-Id: Idb53b4b1cb0de305e1ea011cbd5cca9dc9359014

--- 9.2/docker-entrypoint.sh
+++ 9.2/docker-entrypoint.sh
@@ -1,5 +1,23 @@
-#!/bin/bash
-set -e
+#!/bin/bash -eu
+#
+# Copyright (c) 2017, Google Inc.
+#
+# Permission to use, copy,
+# modify, and distribute this software and its documentation for any purpose,
+# without fee, and without a written agreement is hereby granted, provided that
+# the above copyright notice and this paragraph and the following two paragraphs
+# appear in all copies.
+#
+# IN NO EVENT SHALL GOOGLE BE LIABLE TO ANY PARTY
+# FOR DIRECT, INDIRECT, SPECIAL, INCIDENTAL, OR CONSEQUENTIAL DAMAGES, INCLUDING
+# LOST PROFITS, ARISING OUT OF THE USE OF THIS SOFTWARE AND ITS DOCUMENTATION,
+# EVEN IF GOOGLE HAS BEEN ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
+#
+# GOOGLE SPECIFICALLY DISCLAIMS ANY WARRANTIES, INCLUDING, BUT
+# NOT LIMITED TO, THE IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A
+# PARTICULAR PURPOSE. THE SOFTWARE PROVIDED HEREUNDER IS ON AN "AS IS" BASIS,
+# AND GOOGLE HAS NO OBLIGATIONS TO PROVIDE MAINTENANCE, SUPPORT, UPDATES,
+# ENHANCEMENTS, OR MODIFICATIONS.
 
 # usage: file_env VAR [DEFAULT]
 #    ie: file_env 'XYZ_DB_PASSWORD' 'example'
