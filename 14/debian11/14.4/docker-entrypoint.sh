ef086ec6381c83a4b8ca7d3cb243c3cd493a1cf1
Armando Miani <armadom@google.com>
2022-07-27 09:47:48 +0200

    Fix templates

--- 14/debian11/14.4/docker-entrypoint.sh
+++ 14/debian11/14.4/docker-entrypoint.sh
@@ -23,9 +23,6 @@
 #    ie: file_env 'XYZ_DB_PASSWORD' 'example'
 # (will allow for "$XYZ_DB_PASSWORD_FILE" to fill in the value of
 #  "$XYZ_DB_PASSWORD" from a file, especially for Docker's secrets feature)
-
-set -x
-
 file_env() {
 	local var="$1"
 	local fileVar="${var}_FILE"
